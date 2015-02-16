1ec9168a6db7f853cd3faae2f76e7478a8d3b3f6
ripexz <ripexz@gmail.com>
2015-02-16 21:28:58 +0000

    Starting fires

---
+++ Assets/startFire.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class startFire : MonoBehaviour {
+
+	public Transform fire;
+	private bool fireStarted = false;
+
+	void StartFire(Vector3 location) {
+		if (!fireStarted) {
+			location.y = 0.2f; // ground the fire
+			fire = (Transform) Instantiate(fire, location, transform.rotation);
+			fireStarted = true;
+		}
+	}
+
+	// Update is called once per frame
+	void Update() {
+		if (Input.GetMouseButton(0)) {
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			if (Physics.Raycast(ray, out hit)) {
+				StartFire(hit.point);
+			}
+		}
+	}
+}
