c5be60e6e54cd6c83c06b2c79a77f42512afdd6a
ripexz <ripexz@gmail.com>
2015-03-12 18:40:54 +0000

    Minor tweak, metadata

--- Assets/startFire.cs
+++ Assets/startFire.cs
@@ -22,7 +22,7 @@ public class startFire : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update() {
-		if (Input.GetMouseButton(0)) {
+		if (Input.GetMouseButtonDown(0)) {
 			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 			RaycastHit hit;
 			if (Physics.Raycast(ray, out hit)) {
